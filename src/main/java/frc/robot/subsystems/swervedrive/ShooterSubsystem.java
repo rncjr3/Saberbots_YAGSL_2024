5553cfc423c606e03a996f44da9c1978248e7ed9
Hector Corpus <hectorcorpus121505@gmail.com>
2024-03-20 13:07:18 -0500

    add shooter

--- src/main/java/frc/robot/subsystems/swervedrive/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/ShooterSubsystem.java
@@ -2,10 +2,14 @@ package frc.robot.subsystems.swervedrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj.Timer;
+
 
 public class ShooterSubsystem extends SubsystemBase{
     private CANSparkMax TopShooterMotor = new CANSparkMax(15,MotorType.kBrushless);
     private CANSparkMax BottomShooterMotor = new CANSparkMax(14,MotorType.kBrushless);
+    Timer timer = new Timer();
+
     public ShooterSubsystem(){
         TopShooterMotor.setSmartCurrentLimit(80);
         BottomShooterMotor.setSmartCurrentLimit(80);
@@ -25,11 +29,36 @@ public class ShooterSubsystem extends SubsystemBase{
     }
 
     public void intake(){
+        
         TopShooterMotor.set(-0.15);
         BottomShooterMotor.set(0.15);
     }
 
     public void stopShooter(){
+        TopShooterMotor.set(0);
+        BottomShooterMotor.set(0);
+    }
+
+    public void shootSpeaker(){
+        timer.reset();
+        timer.start();
+        while(timer.get()<5){
+            TopShooterMotor.set(1);
+            if(timer.get()>3){
+                BottomShooterMotor.set(-1);
+            }
+        }
+        TopShooterMotor.set(0);
+        BottomShooterMotor.set(0);
+    }
+
+    public void shootAMP(){
+        timer.reset();
+        timer.start();
+        while(timer.get()<2){
+            TopShooterMotor.set(0.15);
+            BottomShooterMotor.set(-0.15);
+        }
         TopShooterMotor.set(0);
         BottomShooterMotor.set(0);
     }
